c1eead9340b7f96891f184e73e08ce93faa58f0e
Sven Bachmann <gitorious@mcbachmann.de>
2011-02-17 11:03:32 +0100

    all: added GPLv2 headers

--- smart_leds/smart_leds.sh
+++ smart_leds/smart_leds.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# SMART LEDs
+# - Set the drive LED status depend on the SMART status. -
+# Copyright (C) 2011, Sven Bachmann <dev@mcbachmann.de>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
 DRIVES=( sdc sdd sde sdf )
 LEDS=( hdd1 hdd2 hdd3 hdd4 )
